70767d209bc644ae94c7df8fbe9bceba0b290f57
eyeless <maxim.chevychelov@gmail.com>
2022-12-07 13:00:52 +0500

    еще несколько методов для клиента

--- http.py
+++ http.py
@@ -4,26 +4,50 @@ from enum import Enum
 from typing import Dict, Optional, List, Set
 
 
+class Protocol(Enum):
+    HTTP = 0
+    HTTPS = 1
+
+
 class HttpClient:
     RECEIVE_CHUNK_SIZE = 4096
-    BLOCK_BOUNDARY = '0775995b3f2742db7e88858fbb2ede2b'
 
-    class Method(Enum):
-        GET = 'GET'
-        POST = 'POST'
-
-    def __init__(self, host):
+    def __init__(self, host, timeout=2.0, protocol: Protocol = Protocol.HTTP):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
-        self.socket.connect((socket.gethostbyname(host), 80))
+        port = 80 if protocol is Protocol.HTTP else 443
+        self.socket.connect((socket.gethostbyname(host), port))
+        self.socket.settimeout(timeout)
         self.response = None
         self.response_body = None
 
-    def send(self,
-             method: Method,
+    def put(self,
+            http_protocol: str = 'HTTP/1.2',
+            domain: str = '/',
+            to_json: str = False,
+            file_data: Dict[str, str] = None,
+            cookies: Dict[str, str] = None,
+            headers: Dict[str, str] = None) -> str:
+
+        request_body_string, headers_string = self._format_headers_and_requests(cookies, headers, file_data)
+        request = f"PUT {domain} {http_protocol}\r\n" \
+                  f"Host: {self.host}\r\n" \
+                  f"{headers_string if headers_string else ''}" \
+                  f"\r\n\r\n" \
+                  f"{request_body_string if request_body_string else ''}"
+        request = request.encode('utf-8')
+
+        self._send(request)
+        self.response, self.response_body = self._receive()
+        if to_json:
+            with open(to_json, "w") as f:
+                json.dump(self.response, f, ensure_ascii=False, indent=4)
+        return self.response
+
+    def head(self,
              http_protocol: str = 'HTTP/1.2',
              domain: str = '/',
-             to_json: bool = False,
+             to_json: str = False,
              url_parameters: Dict[str, str] = None,
              form_data: Dict[str, str] = None,
              file_data: Dict[str, str] = None,
@@ -32,40 +56,70 @@ class HttpClient:
 
         if url_parameters is not None:
             domain += f"?{self._join_dict(url_parameters, '=', '&')}"
-
-        request_body_string = ''
-        if file_data is not None:
-            request_body_string = self._file_data_to_string(file_data)
-        elif form_data is not None:
-            request_body_string = self._join_dict(form_data, '=', '&')
-
-        if cookies is None:
-            cookies = {}
-        if headers is None:
-            headers = {}
-        if file_data is not None:
-            headers['Content-Type'] = f'multipart/form-data; ' \
-                                      f'boundary={self.BLOCK_BOUNDARY}'
-        elif url_parameters is not None or form_data is not None:
-            headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        if method == self.Method.POST:
-            headers['Content-Length'] = len(request_body_string.encode('utf-8'))
-        if cookies:
-            headers['Cookie'] = self._join_dict(cookies, '=', '; ')
-        headers_string = self._join_dict(headers, ': ', '\r\n')
-
-        request = f"{method.value} {domain} {http_protocol}\r\n" \
-                  f"Host: {self.host}\r\n"\
+        request_body_string, headers_string = self._format_headers_and_requests(cookies, headers, file_data, form_data)
+        request = f"HEAD {domain} {http_protocol}\r\n" \
                   f"{headers_string if headers_string else ''}" \
                   f"\r\n\r\n" \
                   f"{request_body_string if request_body_string else ''}"
-        print(request)
         request = request.encode('utf-8')
 
         self._send(request)
         self.response, self.response_body = self._receive()
         if to_json:
-            with open("response.json", "w") as f:
+            with open(to_json, "w") as f:
+                json.dump(self.response, f, ensure_ascii=False, indent=4)
+        return self.response
+
+    def get(self,
+            http_protocol: str = 'HTTP/1.2',
+            domain: str = '/',
+            to_json: str = False,
+            url_parameters: Dict[str, str] = None,
+            file_data: Dict[str, str] = None,
+            form_data: Dict[str, str] = None,
+            cookies: Dict[str, str] = None,
+            headers: Dict[str, str] = None) -> str:
+        if url_parameters is not None:
+            domain += f"?{self._join_dict(url_parameters, '=', '&')}"
+        request_body_string, headers_string = self._format_headers_and_requests(cookies, headers, file_data, form_data)
+
+        request = f"GET {domain} {http_protocol}\r\n" \
+                  f"{headers_string if headers_string else ''}" \
+                  f"\r\n\r\n" \
+                  f"{request_body_string if request_body_string else ''}"
+        request = request.encode('utf-8')
+
+        self._send(request)
+        self.response, self.response_body = self._receive()
+        if to_json:
+            with open(to_json, "w") as f:
+                json.dump(self.response, f, ensure_ascii=False, indent=4)
+        return self.response
+
+    def post(self,
+             http_protocol: str = 'HTTP/1.2',
+             domain: str = '/',
+             to_json: str = False,
+             url_parameters: Dict[str, str] = None,
+             file_data: Dict[str, str] = None,
+             form_data: Dict[str, str] = None,
+             cookies: Dict[str, str] = None,
+             headers: Dict[str, str] = None) -> str:
+
+        if url_parameters is not None:
+            domain += f"?{self._join_dict(url_parameters, '=', '&')}"
+        request_body_string, headers_string = self._format_headers_and_requests(cookies, headers, file_data, form_data)
+
+        request = f"POST {domain} {http_protocol}\r\n" \
+                  f"{headers_string if headers_string else ''}" \
+                  f"\r\n\r\n" \
+                  f"{request_body_string if request_body_string else ''}"
+        request = request.encode('utf-8')
+
+        self._send(request)
+        self.response, self.response_body = self._receive()
+        if to_json:
+            with open(to_json, "w") as f:
                 json.dump(self.response, f, ensure_ascii=False, indent=4)
         return self.response
 
@@ -81,6 +135,23 @@ class HttpClient:
                    f'\r\n\r\n' \
                    f'{file_data[file_name]}'
         return res[2:] + f'\r\n--{HttpClient.BLOCK_BOUNDARY}--\r\n'
+
+    def _format_headers_and_requests(self, cookies, heads, file_data=None, form_data=None):
+        request_body_string = ''
+        if file_data is not None:
+            request_body_string = self._file_data_to_string(file_data)
+        elif form_data is not None:
+            request_body_string = self._join_dict(form_data, '=', '&')
+
+        if cookies is None:
+            cookies = {}
+        if heads is None:
+            headers = {}
+        heads['Content-Length'] = len(request_body_string.encode('utf-8'))
+        if cookies:
+            heads['Cookie'] = self._join_dict(cookies, '=', '; ')
+        headers_string = self._join_dict(heads, ': ', '\r\n')
+        return request_body_string, headers_string
 
     @staticmethod
     def _join_dict(d: Dict[str, str], key_val_sep: str, entry_sep: str) -> str:
@@ -135,8 +206,10 @@ class HttpClient:
 def main():
     sender = HttpClient("httpbin.org")
 
-    sender.send(sender.Method.GET, to_json=True)
+    # sender.put(domain="", to_json="response.json", headers={"Host": "httpbin.org"})
+    sender.get(to_json="response.json", headers={"Host": "httpbin.org"})
     print(sender.response)
+    sender.close()
 
 
 if __name__ == "__main__":
